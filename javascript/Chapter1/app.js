dc8fcd6e07858d3fde2436bc6b49b3370dd9527a
Muhammad Faraz Ansar <49414010+muhammadfaraz6640@users.noreply.github.com>
2020-06-02 00:32:29 +0500

    Javascript Assignment 1 from chapter 1 to 10 of smarter way to learn javascript

---
+++ javascript/Chapter1/app.js
@@ -0,0 +1,14 @@
+function Question1(){
+    alert('Hello , World..!');
+}
+function Question2(){
+    alert('Error!Please Enter a valid password');
+}
+
+function Question3(){
+    alert('Hey Welcome to js land \n Happy coding');
+}
+function Question4(){
+    alert('Hey Welcome to js land');    
+    alert('Happy coding');
+    }
