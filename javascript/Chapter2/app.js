676f97ed3ff22fb61b40f02099b54bbfb6f0c350
Muhammad Faraz Ansar <49414010+muhammadfaraz6640@users.noreply.github.com>
2020-06-02 00:58:13 +0500

    Chapter 2

---
+++ javascript/Chapter2/app.js
@@ -0,0 +1,42 @@
+//Question 1
+var username;
+
+//question 2
+var my_name = "Muhammad Faraz Ansar";
+
+//question3
+function question3(){
+var message;
+message = "Hello World";
+alert(message);
+}
+function question4(){
+    var name = "Jhon";
+    var age = "15";
+    var course = "Mobile and Web developement course";
+    alert(name);
+    alert(age+' years old');
+    alert(course);
+}
+function question5(){
+    var message;
+    message = "PIZZA";
+    alert(message+'\n'+'PIZZ'+'\n'+'PIZ'+'\n'+'PI'+'\n'+'p');
+    }
+function question6(){
+    var email;
+    message = "muhammad.faraz9@yahoo.com";
+    alert('My Email is : '+message);
+    }
+function question7(){
+    var Book;
+    Book = "A Smarter way to learn Javascript";
+    alert('I am trying to learn the book : '+Book);
+    }
+function question8(){
+    document.write('Yah i Can write html through js');
+    }
+function question9(){
+var random = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+alert(random);
+}
