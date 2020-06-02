777deb7373ec1fb9f288da71691288127903ae27
Muhammad Faraz Ansar <49414010+muhammadfaraz6640@users.noreply.github.com>
2020-06-02 23:58:50 +0500

    Chapter 3 of smarter way to learn python

---
+++ javascript/Chapter3/app.js
@@ -0,0 +1,20 @@
+function Q1(){
+    var age; //declare
+    age = 15; //assign
+    alert('I am '+age+' Years Old');
+}
+function Q2(){
+    var visits;
+    visits = 14;
+    alert('You have visited this website '+visits+' time');    
+}
+function Q3(){
+    var DateBirth = 1999;
+    document.write("My birth year is "+DateBirth);
+}
+function Q4(){
+    var name = "muhammad faraz";
+    var title = "Branded watch";
+    var quantity = "1";
+    document.write(name+" have purchased "+quantity+" "+title);
+}
